b5a862cb85b21627f3b159cac6c482683e1e99a2
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-21 13:20:41 +0100

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # A simple resource in puppet
 file { '/tmp/school':
-  ensure: file
-  owner: www-data
-  group: www-data
-  mode: 0744
+  ensure => file
+  owner => www-data
+  group => www-data
+  mode => 0744
   content => "I love Puppet"
 }
