e0c790d17aa7977681285b3de52bd4e852fd8887
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-21 15:43:50 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
 # This file installs flask python framework using pip3
+package { 'python3-pip':
+  ensure => 'present',
+}
+
 package { 'flask':
   ensure  => '2.1.0',
   provider  => 'pip3',
+  require  => Package['python3-pip'],
 }
