a8382998acbaa9f27b9b23899e68bc7824bb8eb1
iAbasekeme <abasiekemeetim20@gmail.com>
2024-01-23 03:40:11 -0800

    fixed wrong indentation

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,7 @@
 # A manifest that kills a process named killmenow.
-exec { '/usr/bin/pkill killmenow':
+exec { 'pkill killmenow':
   refreshonly => true,
   onlyif      => '/usr/bin/pgrep killmenow',
+  path        => '/usr/bin/',
 }
+
