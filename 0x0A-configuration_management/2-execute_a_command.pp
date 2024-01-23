466d883cfbdd93f8a8de0ae7083ee8f87247c8a0
iAbasekeme <abasiekemeetim20@gmail.com>
2024-01-23 03:26:55 -0800

    fixed lint error

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # A manifest that kills a process named killmenow.
 exec {'/usr/bin/pkill killmenow':
-  refreshonly  => true,
-  onlyif  => '/usr/bin/pgrep killmenow',
+  refreshonly => true,
+  onlyif      => '/usr/bin/pgrep killmenow',
 }
