b5cbe01f7f8236c0461385daba630529a60c09cf
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-31 01:47:25 +0100

    Create 100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# SSH configuration using Puppet
+
+file { '/etc/ssh/ssh_config':
+  ensure  => present,
+  content => "
+    # SSH client configuration
+    Host *
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no
+  ",
+}
