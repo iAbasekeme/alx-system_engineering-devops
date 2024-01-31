9a783ba7dca72d2c24bbdb34ef148fd8a61034b3
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-31 01:44:44 +0100

    Update 2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,6 @@
-# A manifest that kills a process named killmenow.
-exec { 'pkill killmenow':
-  refreshonly => true,
-  onlyif      => '/usr/bin/pgrep killmenow',
-  path        => '/usr/bin/',
-}
+#  create a manifest that kills a process named killmenow
 
+exec { 'pkill':
+  command  => 'pkill -f killmenow',
+  provider => 'shell',
+}
