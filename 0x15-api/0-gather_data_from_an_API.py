cbad560774dedecd874b70d2499ebb31dab88f00
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-02-19 20:14:06 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     user_id = argv[1]
     url_str = 'https://jsonplaceholder.typicode.com/'
     user_str = '{}users/{}'.format(url_str, user_id)
-    todos_str = '{}users/{}/todos'.format(url_str, user_id,)
+    todos_str = '{}users/{}/todos'.format(url_str, user_id)
     employee_str = 'Employee {} is done with tasks'
 
 
