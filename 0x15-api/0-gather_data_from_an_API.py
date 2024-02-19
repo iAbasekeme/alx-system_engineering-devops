d2aaa07f1f5d988dff23d67504a17ece200c0162
iAbasekeme <abasiekemeetim20@gmail.com>
2024-02-19 18:01:02 +0100

    exe

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,10 +2,21 @@
 """Python script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
-import sys
+from sys import argv
 import requests
 
 response = requests.get(
-    'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}')
+    f'https://jsonplaceholder.typicode.com/users/{argv[1]}')
+if response.status_code == 200:
+    data = response.json()
+print('Employee {} is done with tasks'.format(data['name']))
 
-print(response.content)
+todos_res = requests.get(
+    f'https://jsonplaceholder.typicode.com/users/{argv[1]}/todos')
+if response.status_code == 200:
+    data = todos_res.json()
+    count = 0
+    for dat in list(data):
+        if dat['completed'] == True:
+            count += 1
+    print('({}/'.format(count))
