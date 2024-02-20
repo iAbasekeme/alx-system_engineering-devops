4a66c51812fc87fc2bfa56fcc79ef74ae3b68770
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-02-20 13:38:20 +0100

    Create 2-export_to_JSON.py

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" A script that export data in the json format.
+"""
+import csv
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    user_id = argv[1]
+    url_str = 'https://jsonplaceholder.typicode.com/'
+    user_str = '{}users/{}'.format(url_str, user_id)
+    todos_str = '{}users/{}/todos'.format(url_str, user_id)
+    file = '{}.json'.format(user_id)
+
+    res = requests.get(user_str)
+    print(res)
