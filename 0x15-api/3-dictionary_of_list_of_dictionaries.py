c5bb2420dcd8ceecc0fdb477363b7cdb0133b1bb
iAbasekeme <abasiekemeetim20@gmail.com>
2024-02-20 20:03:29 +0100

    fixed wrong indentations

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -10,11 +10,18 @@ if __name__ == '__main__':
     url_str = 'https://jsonplaceholder.typicode.com/'
     user_str = '{}users'.format(url_str)
     todos_str = '{}/todos'.format(url_str)
+    first_dict = {}
+
 
     res = requests.get(user_str).json()
-    for res in users:
-        print(users)
 
-    res_todos = requests.get(todos_str)
-    data = res_todos.json()
-    print(data)
+    res_todos = requests.get(todos_str).json()
+    for users in res:
+        tasks_list = []
+        for todos in res_todos:
+            if users['id'] == todos['userId']:
+                task_dict = {"username": users['username'], "task": todos['title'], "completed": todos['completed']}
+                tasks_list.append(task_dict)
+        first_dict[users['id']] = tasks_list
+    with open('todo_all_employees.json', 'w') as file:
+        json.dump(first_dict, file)
