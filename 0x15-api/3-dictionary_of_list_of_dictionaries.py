1eb0a21174341504153a9498f34bf08fb4a209de
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-02-20 17:21:15 +0100

    Update 3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,8 +7,8 @@ import requests
 from sys import argv
 
 if __name__ == '__main__':
-    user_id = '{}/users'.format(url_str)
     url_str = 'https://jsonplaceholder.typicode.com/'
+    user_id = '{}/users'.format(url_str)
     user_str = '{}users/{}'.format(url_str, user_id)
     todos_str = '{}users/{}/todos'.format(url_str, user_id)
     file = '{}.json'.format(user_id)
