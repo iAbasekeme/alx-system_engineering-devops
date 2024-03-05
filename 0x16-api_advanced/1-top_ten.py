52afdbc4a60fae5a10f06f3eb75c5634dd9687b7
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-03-05 23:51:41 +0100

    fixed wrong indentation

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,22 +5,22 @@ import requests
 
 
 def top_ten(subreddit):
-  '''A function that sends a request queries the reddit api and prints the
+    '''A function that sends a request queries the reddit api and prints the
     title of the first ten hot posts
-  '''
-  url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-  header = {
-      'Accept': 'application/json',
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
-  }
-  response = requests.get(url, headers=header, allow_redirects=False)
-  if response.status_code != 200:
-    return None
+    '''
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    header = {
+        'Accept': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+    }
+    response = requests.get(url, headers=header, allow_redirects=False)
+    if response.status_code != 200:
+        return None
 
-  try:
-    data = response.json()
-    posts = data['data']['children'][0:10]
-    for post in posts:
-      print(post['data']['title'])
-  except (KeyError, ValueError):
-    return None
+    try:
+        data = response.json()
+        posts = data['data']['children'][0:10]
+        for post in posts:
+            print(post['data']['title'])
+    except (KeyError, ValueError):
+        return None
