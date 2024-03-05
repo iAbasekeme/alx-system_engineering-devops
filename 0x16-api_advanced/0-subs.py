b5d0e9fc4717d55b9a9c096873e6f2e95c68e06c
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-03-05 14:43:33 +0100

    Create 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''A file that sends a request to the reddit Api
+'''
+import requests
+
+
+def number_of_subscribers(subreddit):
+  '''A function that makes a request and returns subscribers
+  '''
+  url = f"https://www.reddit.com/r/{subreddit}/about.json"
+  header = {
+      'Accept': 'application/json',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+  }
+  response = requests.get(url, header=headers, allow_redirects=False)
+  if response.status_code != 200:
+    return 0
+
+  try:
+    data = response.json()
+    subscribers = data['data']['subscribers']
+    return subscribers
+  except (KeyError, ValueError):
+    return 0
