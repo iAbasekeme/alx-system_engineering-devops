05564b37009e047179c554f5c478ab5ec2162c1b
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-03-05 23:51:52 +0100

    fixed wrong indentation

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,20 +5,20 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-  '''A function that makes a request and returns subscribers
-  '''
-  url = f"https://www.reddit.com/r/{subreddit}/about.json"
-  header = {
-      'Accept': 'application/json',
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
-  }
-  response = requests.get(url, headers=header, allow_redirects=False)
-  if response.status_code != 200:
-    return 0
+    '''A function that makes a request and returns subscribers
+    '''
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    header = {
+        'Accept': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+    }
+    response = requests.get(url, headers=header, allow_redirects=False)
+    if response.status_code != 200:
+      return 0
 
-  try:
-    data = response.json()
-    subscribers = data['data']['subscribers']
-    return subscribers
-  except (KeyError, ValueError):
-    return 0
+    try:
+      data = response.json()
+      subscribers = data['data']['subscribers']
+      return subscribers
+    except (KeyError, ValueError):
+      return 0
